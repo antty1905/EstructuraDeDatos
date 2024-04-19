250dc0b2bea0f39bac5cd60db976c9cc3b58ca1e
antty1905 <antonyjuniorhuamaniramos@gmail.com>
2024-04-19 12:00:28 -0500

    Cambio lab2

---
+++ CursoEstructuraDeDatos2024/Laboratorio2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Programa qe sume los n primeros numeros
+
+var n=50;
+var suma = 0;
+for (var i = 1; i <=n; i++)
+{
+    suma = suma + i;
+}
+
+
+
+
+Console.WriteLine("El resultado es :"+ suma.ToString());
